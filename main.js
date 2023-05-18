85e3d4ca035727b58dc5a373c00d4213009382fe
Manuell00 <manuell.caselli@gmail.com>
2023-05-18 16:28:12 +0200

    Inserimento button + img

--- main.js
+++ main.js
@@ -4,9 +4,30 @@
 const {createApp} = Vue
 
 createApp({
+
     data(){
         return{
-            message : 'Hello'
+            title : '',
+            verde : 'text-green mb-3',
+            url : "https://unsplash.it/600/300?image=171"
+        }
+    },
+
+    // Inserisco le funzioni
+    methods: {
+        generaTitolo : function () {
+            let img = document.getElementById("randomImg")
+
+            if (img.classList.contains("styleNone")) {
+                img.classList.remove("styleNone")
+                img.classList.add("styleFlex")
+            }
+            
+            else{
+                img.classList.remove("styleFlex")
+                img.classList.add("styleNone")
+            }
+            
         }
     }
-}).mount('#title')
+}).mount('#container');
